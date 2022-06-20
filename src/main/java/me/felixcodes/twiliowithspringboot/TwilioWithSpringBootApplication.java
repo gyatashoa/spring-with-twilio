cd33bc42c997b2ba7617d7df65c80892c4512eb3
Asamoah Yeboah Felix <felixasamoah539@gmail.com>
2022-06-20 05:27:28 +0000

    initial setup

---
+++ src/main/java/me/felixcodes/twiliowithspringboot/TwilioWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package me.felixcodes.twiliowithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwilioWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwilioWithSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/felixcodes/twiliowithspringboot/TwilioWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package me.felixcodes.twiliowithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwilioWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

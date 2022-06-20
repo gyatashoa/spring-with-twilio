0453aa713539effc01d6d5921659db9f19baeb19
gyatashoa <felixasamoah539@gmail.com>
2022-06-20 07:41:41 +0000

    added mms support

--- src/main/java/me/felixcodes/twiliowithspringboot/controllers/SMSController.java
+++ src/main/java/me/felixcodes/twiliowithspringboot/controllers/SMSController.java
@@ -1,23 +1,38 @@
 package me.felixcodes.twiliowithspringboot.controllers;
 
 
-import lombok.AllArgsConstructor;
 import me.felixcodes.twiliowithspringboot.dto.SMSRequest;
 import me.felixcodes.twiliowithspringboot.services.SMSService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/sms")
-@AllArgsConstructor
 public class SMSController {
 
     final SMSService smsService;
 
+    public SMSController(SMSService smsService) {
+        this.smsService = smsService;
+    }
+
     @PostMapping("/send")
     public void sms(@RequestBody SMSRequest smsRequest){
         smsService.sendSms(smsRequest);
+    }
+
+    @PostMapping("/media")
+    public void mms(@RequestBody SMSRequest smsRequest,@RequestHeader String host) throws IOException {
+        smsService.sendMMS(smsRequest,host);
+    }
+
+    @GetMapping(value = "/image",
+        produces = MediaType.IMAGE_PNG_VALUE
+    )
+    public @ResponseBody byte[] getImage() throws IOException {
+        return smsService.getImage();
     }
 }
--- src/main/java/me/felixcodes/twiliowithspringboot/services/SMSService.java
+++ src/main/java/me/felixcodes/twiliowithspringboot/services/SMSService.java
@@ -1,20 +1,30 @@
 package me.felixcodes.twiliowithspringboot.services;
 
-import com.twilio.Twilio;
+import com.twilio.converter.Promoter;
 import com.twilio.rest.api.v2010.account.Message;
-import com.twilio.type.PhoneNumber;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.felixcodes.twiliowithspringboot.config.TwilioConfig;
 import me.felixcodes.twiliowithspringboot.dto.SMSRequest;
+import org.apache.commons.io.IOUtils;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+
 @Service
-@AllArgsConstructor
 @Slf4j
 public class SMSService {
 
     final TwilioConfig twilioConfig;
+
+    public SMSService(TwilioConfig twilioConfig) {
+        this.twilioConfig = twilioConfig;
+    }
 
     public void sendSms(SMSRequest smsRequest){
         Message message = Message.creator(
@@ -24,5 +34,23 @@ public class SMSService {
         ).create();
         var status = message.getStatus();
         log.info(status.name());
+    }
+
+    public void sendMMS(SMSRequest smsRequest, String host) throws IOException {
+        Resource resource = new ClassPathResource("pic.png");
+        Message message = Message.creator(
+                new com.twilio.type.PhoneNumber(smsRequest.phoneNumber()),
+                new com.twilio.type.PhoneNumber(twilioConfig.TWILIO_PHONE_NUMBER),
+                "You have a new order request on, please open the app to check"
+        ).setMediaUrl(
+                URI.create("https://freeiconshop.com/wp-content/uploads/edd/image-solid.png")
+        ).create();
+        var status = message.getStatus();
+        log.info(status.name());
+    }
+
+    public byte[] getImage() throws IOException {
+        Resource resource = new ClassPathResource("pic.png");
+        return IOUtils.toByteArray(resource.getInputStream());
     }
 }
